e06afcdd4f1ade2b04bcc94acda6c44a96e745a6
vasreb <vasreborn@gmail.com>
2019-05-26 14:23:47 +0300

    combineReducers

--- src/reducers/main.jsx
+++ src/reducers/main.jsx
@@ -1,15 +1,14 @@
+import { combineReducers } from 'redux'
 import photos from './photos';
 import isLoading from './isLoading';
 import currentAlbumId from './currentAlbumId';
 import currentPhoto from './currentPhoto';
 import isError from './isError';
 
-export default function appReducer(state={}, action) {
-	return {
-        photos: photos(state.photos, action),
-        isLoading: isLoading(state.isLoading, action),
-        currentAlbumId: currentAlbumId(state.currentAlbumId, action),
-        currentPhoto: currentPhoto(state.currentPhoto, action),
-        isError: isError(state.isError, action)
-	}
-}
+export default combineReducers({
+        photos,
+        isLoading,
+        currentAlbumId,
+        currentPhoto,
+        isError
+})
